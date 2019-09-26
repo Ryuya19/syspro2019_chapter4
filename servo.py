7ea8d77f7303695e3f2bef71804d300157315f2c
Ryuya19 <s16025@tokyo.kosen-ac.jp>
2019-09-26 04:23:29 +0100

    PWMのモータ制御

--- servo.py
+++ servo.py
@@ -3,23 +3,16 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(2, GPIO.OUT)
-servo = GPIO.PWM(2, 50)
-servo.start(0.0)
 
-bottom = 2.5
-middle = 7.2
-top = 12.0
-
-for i in range(5):
-	servo.ChangeDutyCycle(bottom)
-	time.sleep(1.0)
-
-	servo.ChangeDutyCycle(middle)
-	time.sleep(1.0)
-
-	servo.ChangeDutyCycle(top)
-	time.sleep(1.0)
+def setservo(deg):
+    servo = GPIO.PWM(2, 50)
+    servo.start(0.0)
+    servo.ChangeDutyCycle(((deg + 90) / 180 + 0.5) * 5)
+    time.sleep(1)
 
 
+for i in range(10):
+    setservo(-90)
+    setservo(90)
 
-
+GPIO.cleanup()
